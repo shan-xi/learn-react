ea303e62085546cda69a7c627d9bb8e797696d15
廖善璽 <liaoshanxi@liaoshanxideMacBook-Pro.local>
2024-01-21 23:45:58 +0800

    export and import component not finish yet

--- describing-the-ui/your-first-component/src/App.js
+++ describing-the-ui/your-first-component/src/App.js
@@ -1,14 +1,5 @@
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
-}
+import Gallery from "./Gallery.js";
 
-export default function Gallery() {
-  return (
-    <>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </>
-  );
+export default function App() {
+  return <Gallery />;
 }
