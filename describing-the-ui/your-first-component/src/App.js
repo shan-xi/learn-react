ce9c36df84cf442cb693b9a04898e22b908a49dc
spinliaomarais <spinliao@storemarais.com>
2024-01-22 17:22:42 +0800

    Importing and Exporting Components

--- describing-the-ui/your-first-component/src/App.js
+++ describing-the-ui/your-first-component/src/App.js
@@ -1,5 +1,11 @@
 import Gallery from "./Gallery.js";
+import Profile from "./Profile.js";
 
 export default function App() {
-  return <Gallery />;
+  return (
+    <div>
+      <Profile />
+      <Gallery />
+    </div>
+  );
 }
--- describing-the-ui/your-first-component/src/Gallery.js
+++ describing-the-ui/your-first-component/src/Gallery.js
@@ -1,14 +1,12 @@
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
-}
+import Profile from "./Profile.js";
 
 export default function Gallery() {
   return (
-    <>
+    <section>
       <h1>Amazing scientists</h1>
       <Profile />
       <Profile />
       <Profile />
-    </>
+    </section>
   );
 }
